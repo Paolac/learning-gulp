b42e494ec12aff5ef5f1cbc0c5e1551183b71ae8
paolac <paola_citlalli@hotmail.com>
2015-08-12 17:49:04 -0500

    JSon monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,10 @@ var jsSources = [
 
 var sassSources =['components/sass/style.scss'];
 
+var htmlSources = ['builds/development/*.html'];
+
+var jsonSources = ['builds/development/js/*.json'];
+
 gulp.task('coffee',function(){
 	gulp.src(coffeeSources)
 		.pipe(coffee({bare:true})
@@ -52,6 +56,9 @@ gulp.task('watch' , function (){
 	gulp.watch(coffeeSources,['coffee']);
 	gulp.watch(jsSources,['js']);
 	gulp.watch('components/sass/*.scss',['compass']);
+	gulp.watch(htmlSources,['html']);
+	gulp.watch('builds/development/js/*.json',['json']);
+
 });
 
 
@@ -61,4 +68,14 @@ gulp.task('connect' , function(){
 		livereload: true
 	});
 });
-gulp.task('default',['coffee','js','compass','connect','watch']);
+
+gulp.task('html',function(){
+	gulp.src(htmlSources)
+	.pipe(connect.reload())
+});
+
+gulp.task('json',function(){
+	gulp.src(jsonSources)
+	.pipe(connect.reload())
+});
+gulp.task('default',['coffee','js','compass','connect','html','json','watch']);
